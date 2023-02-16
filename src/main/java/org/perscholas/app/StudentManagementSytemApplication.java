858dbfe0b9fa5fe31315051190b146ce18e690c9
SeckinDogaroglu <seckindo@hotmail.com>
2023-02-16 09:22:16 -0600

    init commit for project

---
+++ src/main/java/org/perscholas/app/StudentManagementSytemApplication.java
@@ -0,0 +1,13 @@
+package org.perscholas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSytemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSytemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/perscholas/app/StudentManagementSytemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perscholas.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSytemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
